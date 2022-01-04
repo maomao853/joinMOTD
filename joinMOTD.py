6679fdb0ff4a167ffda9a96479b3362b5a83fe03
maomao853 <70410309+maomao853@users.noreply.github.com>
2022-01-04 16:54:12 -0600

    Update joinMOTD.py
    - Remove the need for config file (change to constants)
    - Remove multi-server support
    - Add logger for 'daycount' import errors

--- joinMOTD.py
+++ joinMOTD.py
@@ -1,15 +1,39 @@
-# -*- coding: utf-8 -*-
-from daycount import getday
+from mcdreforged.api.all import *
 
-msgstart = '=====Welcome back to TIS====='
-servername = 'TIS'
-msgend = ''
+PLUGIN_METADATA = {
+	'id': 'join_motd',
+	'version': '1.1.0',
+	'name': 'Join MOTD Display',
+	'author': 'Fallen_Breath',
+	'link': 'https://github.com/TISUnion/joinMOTD'
+}
 
-def onPlayerJoin(server, player):
-  msg = ''
-  msg += msgstart + '\n'
-  msg += '今天是' + servername + '开服的第' + getday() + '天\n'
-  for line in msg.splitlines():
-    server.tell(player, line)
-  server.execute('tellraw ' + player + ' {"text":"点击此处进入创造服","clickEvent":{"action":"run_command","value":"/server creative"}}')
-  server.execute('tellraw ' + player + ' {"text":"点击此处列出任务列表","clickEvent":{"action":"run_command","value":"!!task list"}}')
+Prefix = '!!joinMOTD'
+server_name = 'IFT-Survival'
+main_server_name = 'IFT国际科技联盟'
+
+
+def get_day(server: ServerInterface):
+	api = server.get_plugin_instance('daycount')
+	if hasattr(api, 'getday') and callable(api.getday):
+		return api.getday()
+	try:
+		import daycount
+		return daycount.getday()
+	except Exception as e:
+		server.logger.info(e)
+		return '?'
+
+
+def on_player_joined(server: ServerInterface, player, info):
+	server.tell(player, '§7=======§r Welcome back to §e{}§7 =======§r'.format(server_name))
+	server.tell(player, '今天是§e{}§r开服的第§e{}§r天'.format(main_server_name, get_day(server)))
+
+
+def on_user_info(server, info):
+	if info.content == Prefix:
+		on_player_joined(server, info.player, None)
+
+
+def on_load(server, old):
+	server.register_help_message(Prefix, '显示欢迎消息')
